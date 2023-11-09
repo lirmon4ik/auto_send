e06deb191b9be8e2e2a57ba349d32d8692c31eb5
lirmon4ik <lirmon4ik@yandex.ru>
2023-11-09 21:50:14 +0700

    Реализовал обновление бд приложения

--- updater.py
+++ updater.py
@@ -1,31 +1,24 @@
 import sqlite3
-data=[[123,"Иванов Иван","Иванович","почта@домен"],
-[456,"Иванов Иван2","Иванович",""],
-[78,"Иванов Иван3","Иванович","почта@домен3"],
-[9,"Иванов Иван4","Иванович","почта@домен4"]]
 def update_db():
     connection=sqlite3.connect("my_db.db")
     cursor=connection.cursor()
-    id=int(cursor.execute("select count(*) from users where id_delo not null").fetchall()[0][0])+1
+    cursor.execute("select count(*) from users where id_delo not null")
+    id=int(cursor.fetchall()[0][0])+1
+    cursor.execute("select id_delo from users where id_delo not null")
+    users=cursor.fetchall()
+    users=list(map(lambda x: x[0],users))
     for item in data:
-        cursor.execute(f'''
-        update users
-        set id_delo={item[0]}, firstname="{item[1]}",
-        lastname="{item[2]}", email="{item[3]}"
-        where id_delo is Null 
-        and id={id}
-        and id_delo not in (select id_delo from users where id_delo is not Null)
-        ''')
-        connection.commit()
-        id+=1
-    
-    cursor.execute(f'''
-    select * from users 
-    ''')
-    connection.commit()
-    for item in cursor.fetchall():
-        print(item)
+        print(item[0],users)
+        if item[0] not in users:
+            print(True)
+            cursor.execute(f'''
+            update users
+            set id_delo={item[0]}, firstname="{item[1]}",
+            lastname="{item[2]}", email="{item[3]}"
+            where id_delo is Null 
+            and id={id}
+            ''')
+            id+=1
+            connection.commit()
     connection.close()
-
-update_db()
 
