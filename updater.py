2129e3b74ab631cf0f6aa58f3f728d8f2a9e6077
lirmon4ik <lirmon4ik@yandex.ru>
2023-11-11 02:09:36 +0700

    Реализовано получение данных пользователей из MsSql

--- updater.py
+++ updater.py
@@ -1,16 +1,25 @@
+import pyodbc
 import sqlite3
-def update_db():
+def update_db(server,database,username,password)
+    connectionString = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+    conn = pyodbc.connect(connectionString)
+    SQL_QUERY = """
+        Select distinct А.ID,concat(Имя,' ',Отчество) firstname, Фамилия lastname, E_Mail from Все_Абитуриенты А 
+    join Все_Заявления З on А.ID=З.ID and Год_Набора=year(GetDate()) and ВИ_ДО=1 ;
+        """
+    cur = conn.cursor()
+    cur.execute(SQL_QUERY)
+    data=cur.fetchall()
+    conn.close()
     connection=sqlite3.connect("my_db.db")
     cursor=connection.cursor()
-    cursor.execute("select count(*) from users where id_delo not null")
-    id=int(cursor.fetchall()[0][0])+1
-    cursor.execute("select id_delo from users where id_delo not null")
-    users=cursor.fetchall()
-    users=list(map(lambda x: x[0],users))
     for item in data:
-        print(item[0],users)
+        cursor.execute("select count(*) from users where id_delo not null")
+        id=int(cursor.fetchall()[0][0])+1
+        cursor.execute("select id_delo from users where id_delo not null")
+        users=cursor.fetchall()
+        users=list(map(lambda x: x[0],users))
         if item[0] not in users:
-            print(True)
             cursor.execute(f'''
             update users
             set id_delo={item[0]}, firstname="{item[1]}",
@@ -21,4 +30,3 @@ def update_db():
             id+=1
             connection.commit()
     connection.close()
-
