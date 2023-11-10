4c9e46d55a53696c30b509beb533181651e719e3
Дмитрий <dolgov.mitia2016@yandex.ru>
2023-11-11 05:26:05 +0700

    Добавлена авторизация в БД, кнопка Обновить не работает

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-# -*- coding: cp1251 -*-
+# -*- coding: utf-8 -*-
 
 import dearpygui.dearpygui as dpg
 from win32api import GetSystemMetrics
 import creator_db as c_db
 import os
-#import updater
+import working_with_listbox as wwl
+from updater import update_db
+
 
 os.path.isfile(os.getcwd()+'\\'+'my_db.db')
 
@@ -14,7 +16,7 @@ with dpg.font_registry():
 	with dpg.font("NotoMono-Regular.ttf", 12, default_font=True) as font1:
 
 		# add the default font range
-		dpg.add_font_range_hint(dpg.mvFontRangeHint_Default)
+		dpg.add_font_range_hint(dpg.mvFontRangeHint_Cyrillic)
 
 		# helper to add range of characters
 		#    Options:
@@ -35,27 +37,41 @@ print(width, height)
 
 
 
-def ret_value(sender, data):
+def add_abit(sender, data):
     items = dpg.get_item_configuration("lb_2")['items']
     items.append(data)
     dpg.configure_item("lb_2", items=items)
 
     
     items = dpg.get_item_configuration("lb_1")['items']
-    print(type(items))
     items.remove(data)
     dpg.configure_item("lb_1", items=items)
 
-    dpg.set_value('log', f"Добавлен обьект {data} в lb_2 и удалён из lb_1")
+    dpg.configure_item("log", color=(0,255,0))
+    #dpg.set_value('log', f"Р”РѕР±Р°РІР»РµРЅ Р°Р±РёС‚СѓСЂРёРµРЅС‚: {data} ")
+    dpg.add_text('log',f"Р”РѕР±Р°РІР»РµРЅ Р°Р±РёС‚СѓСЂРёРµРЅС‚: {data} ")
+
+def del_abit(sender, data):
+    items = dpg.get_item_configuration("lb_1")['items']
+    items.append(data)
+    dpg.configure_item("lb_1", items=items)
 
     
+    items = dpg.get_item_configuration("lb_2")['items']
+    items.remove(data)
+    dpg.configure_item("lb_2", items=items)
+
+    dpg.configure_item("log", color=(255,0,0))
+    dpg.set_value('log', f"РђР±РёС‚СѓСЂРёРµРЅС‚ СѓРґР°Р»С‘РЅ: {data} ")
+
 
 def open_file(sender, app_data, user_data):
     #print("Sender: ", sender)
     c_db.create_db()
     c_db.read_csv(app_data['selections'][app_data['file_name']])
     dpg.configure_item("log", color=(0,255,0))
-    dpg.set_value('log', 'Данные занесены в БД')
+    dpg.set_value('log', 'Р”Р°РЅРЅС‹Рµ Р·Р°РЅРµСЃРµРЅС‹ РІ Р‘Р”')
+    
     
     
 dpg.create_viewport(title='Custom Title', 
@@ -70,9 +86,13 @@ dpg.create_viewport(title='Custom Title',
 
 
 with dpg.viewport_menu_bar():
-      with dpg.menu(label="Settings"):
-
-                    dpg.add_menu_item(label="Подключение к БД")
+    with dpg.menu(label="Settings"):
+        with dpg.menu(label="РџРѕРґРєР»СЋС‡РµРЅРёРµ Рє Р‘Р”"):
+            dpg.add_input_text(label="РРјСЏ РЎРµСЂРІРµСЂР°", tag="name")
+            dpg.add_input_text(label="РРјСЏ РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ", tag="login")
+            dpg.add_input_text(label="РџР°СЂРѕР»СЊ",tag="pwd", password=True, hint="<password>")
+            #dpg.add_input_text(label="Р‘Р°Р·Р° РґР°РЅРЅС‹С…",tag="BD")
+            
                     
             
 
@@ -83,15 +103,16 @@ with dpg.viewport_menu_bar():
 with dpg.window(tag="start_window"):
         
         with dpg.group(horizontal=True):
-            dpg.add_listbox(items=['неустроев сергей','долгов дима'], 
+            dpg.add_listbox(items=wwl.get_users(), 
                             tag='lb_1', 
-                            callback=ret_value,
+                            callback=add_abit,
                             width=205,
                             pos=[535,23],
                             num_items=20
                             )
             dpg.add_listbox(items=[],
                             tag='lb_2',
+                            callback=del_abit,
                             width=205,
                             pos=[320,23],
                             num_items=20
@@ -102,25 +123,29 @@ with dpg.window(tag="start_window"):
         with dpg.child_window(width=305, pos=[8,23], height=330):
             dpg.add_text(tag='text_output',wrap=1)
         with dpg.child_window(width=305, pos=[8,360], autosize_y=True):
-            dpg.add_text(tag='log',wrap=250)
+            dpg.add_text(tag='log',wrap=250,color=(0,0,0))
         with dpg.child_window(width=420, pos=[320,360], autosize_y=True):
             with dpg.child_window(width=198, pos=[8,8], autosize_y=True):
                 with dpg.group(horizontal=True):
-                    dpg.add_button(label='Открыть файл', width=94, height=50, tag='open', pos=[1,4],callback=lambda: dpg.show_item("file_dialog_id"))
+                    dpg.add_button(label='РћС‚РєСЂС‹С‚СЊ С„Р°Р№Р»', width=94, height=50, tag='open', pos=[1,4],callback=lambda: dpg.show_item("file_dialog_id"))
                     if os.path.isfile(os.getcwd()+'\\'+'my_db.db'):
                         dpg.configure_item("open", enabled=False)
                         dpg.configure_item("log", color=(0,255,0))
-                        dpg.set_value('log','БД уже создана')
+                        dpg.set_value('log','Р‘Р” СѓР¶Рµ СЃРѕР·РґР°РЅР°')
                     else:
                         dpg.configure_item("log", color=(255,0,0))
-                        dpg.set_value('log','БД не создана или отсутствует, ... Выберите файл')
+                        dpg.set_value('log','Р‘Р” РЅРµ РЅР°Р№РґРµРЅР°, ... Р’С‹Р±РµСЂРёС‚Рµ С„Р°Р№Р» (.*csv)')
                         
                         
-                    dpg.add_button(label='Обновить', tag='update',width=94, height=50)    
+                    dpg.add_button(label='РћР±РЅРѕРІРёС‚СЊ', tag='update',width=94, height=50, callback=lambda: update_db(*dpg.get_values(['name','login','pwd'])))
+                    if dpg.get_value('name')=='':
+                        dpg.configure_item("update", enabled=False)
+                        dpg.configure_item("log", color=(255,0,0))
+                        dpg.set_value('log','РќРµС‚ РїРѕРґРєР»СЋС‡РµРЅРёСЏ Рє Р‘Р”')
+                    else:
+                        dpg.configure_item("update", enabled=True)
         dpg.bind_font(font1)
-            
-        
-            
+                   
     
                         
 
