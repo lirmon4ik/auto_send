0aeb0787fcd6cabee77bfd28ef7e7c116fad514d
lirmon4ik <lirmon4ik@yandex.ru>
2023-11-14 23:32:04 +0700

    Поменяна сортировка listbox

--- working_with_listbox.py
+++ working_with_listbox.py
@@ -6,7 +6,7 @@ def get_users():
         cursor=connection.cursor()
         cursor.execute("select firstname, lastname, number_of_emails_sent, id_delo  from users where id_delo not null")
         users=cursor.fetchall()
-        users=sorted(users,key=lambda x:(x[2],x[0]))
+        users=sorted(users,key=lambda x:(x[2],x[1]))
         users=list(map(lambda x: str(x[2])+" "+x[1]+" "+x[0]+" "+str(x[3]),users))
         connection.close()
         return users
