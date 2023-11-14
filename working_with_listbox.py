20dae112c62ec6b62008b56ad9e338f287e19e42
lirmon4ik <lirmon4ik@yandex.ru>
2023-11-14 22:40:27 +0700

    При отсутствующей базе данных возвращает пустой список для правильного заполнения listbox

--- working_with_listbox.py
+++ working_with_listbox.py
@@ -1,10 +1,14 @@
 import sqlite3
+import os
 def get_users():
-    connection=sqlite3.connect("my_db.db")
-    cursor=connection.cursor()
-    cursor.execute("select firstname, lastname, number_of_emails_sent, id_delo  from users where id_delo not null")
-    users=cursor.fetchall()
-    users=sorted(users,key=lambda x:(x[2],x[0]))
-    users=list(map(lambda x: str(x[2])+" "+x[0]+" "+x[1]+" "+str(x[3]),users))
-    connection.close()
-    return users
+    if "my_db.db" in os.listdir():
+        connection=sqlite3.connect("my_db.db")
+        cursor=connection.cursor()
+        cursor.execute("select firstname, lastname, number_of_emails_sent, id_delo  from users where id_delo not null")
+        users=cursor.fetchall()
+        users=sorted(users,key=lambda x:(x[2],x[0]))
+        users=list(map(lambda x: str(x[2])+" "+x[0]+" "+x[1]+" "+str(x[3]),users))
+        connection.close()
+        return users
+    else:
+        return []
