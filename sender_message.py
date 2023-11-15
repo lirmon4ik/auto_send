00605ea94f8ce01874ffd186275efd9145f19787
lirmon4ik <lirmon4ik@yandex.ru>
2023-11-15 23:35:56 +0700

    Возвращаем список пользователей кому отправлено уведомление(исправлен баг)

--- sender_message.py
+++ sender_message.py
@@ -20,17 +20,17 @@ def send_message_to_users(users,mail_from,password):
     for user in users:
         lastname, firstname, surname, id_delo=user.split()[1:]
         cursor.execute(f"select username, password, email from users where id_delo={id_delo}")
-        users_to_send.append(cursor.fetchall()[0])
+        users_to_send.append((cursor.fetchall()[0],lastname,firstname))
     subject="Логин и пароль для дистанционной сдачи ВИ"
     with open("message.html", encoding="utf-8") as f: 
         message=f.read().strip()
     for user in users_to_send:
-        if user[2]=="None":
+        if user[0][2]=="None":
             users_to_send_complete.append((f"{lastname} {firstname}",False))
             continue
         message_copy=message
-        message_copy=message_copy.replace("login",user[0])
-        message_copy=message_copy.replace("password",user[1])
-        send_email(mail_from,user[2],password,subject,message_copy)
-        users_to_send_complete.append((f"{lastname} {firstname}",True))
+        message_copy=message_copy.replace("login",user[0][0])
+        message_copy=message_copy.replace("password",user[0][1])
+        send_email(mail_from,user[0][2],password,subject,message_copy)
+        users_to_send_complete.append((f"{user[1]} {user[2]}",True))
     return users_to_send_complete
