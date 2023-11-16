eca1e349700e6f2cc0468921c02cc1c7660aef25
Дмитрий <dolgov.mitia2016@yandex.ru>
2023-11-16 16:39:13 +0700

    Переформатирован код полностью, вынесены функции, добавлены коментарии

--- main.py
+++ main.py
@@ -10,33 +10,8 @@ from sender_message import send_message_to_users
 import save_settings as sett
 import threading
 
-dpg.create_context()
 
-settings = sett.Settings()
-
-with dpg.font_registry():
-    with dpg.font("NotoMono-Regular.ttf", 14, default_font=True) as font1:
-
-        # add the default font range
-        dpg.add_font_range_hint(dpg.mvFontRangeHint_Cyrillic)
-
-        # helper to add range of characters
-        #    Options:
-        #        mvFontRangeHint_Japanese
-        #        mvFontRangeHint_Korean
-        #        mvFontRangeHint_Chinese_Full
-        #        mvFontRangeHint_Chinese_Simplified_Common
-        #        mvFontRangeHint_Cyrillic
-        #        mvFontRangeHint_Thai
-        #        mvFontRangeHint_Vietnamese
-        dpg.add_font_range_hint(dpg.mvFontRangeHint_Cyrillic)
-
-
-width = GetSystemMetrics(0)
-height = GetSystemMetrics(1)
-
-
-def add_abit(sender, data):
+def add_abit(data):  # функция добавления абитуриента в список
     items = dpg.get_item_configuration("lb_2")['items']
     items.append(data)
     dpg.configure_item("lb_2", items=items)
@@ -48,7 +23,7 @@ def add_abit(sender, data):
                  parent="text_parent", color=(0, 255, 0))
 
 
-def del_abit(sender, data):
+def del_abit(data):  # функция удаления абитуриента из списка
     items = dpg.get_item_configuration("lb_1")['items']
     items.append(data)
     dpg.configure_item("lb_1", items=items)
@@ -60,7 +35,7 @@ def del_abit(sender, data):
                  parent='text_parent', color=(255, 0, 0))
 
 
-def open_file(sender, app_data, user_data):
+def open_file(app_data):  # функция открытия csv файла
 
     c_db.create_db()
     c_db.read_csv(app_data['selections'][app_data['file_name']])
@@ -69,7 +44,7 @@ def open_file(sender, app_data, user_data):
     dpg.configure_item("open", enabled=False)
 
 
-def send_email():
+def send_email():  # функция отправки писем в почту
     login_email = dpg.get_value("login_email")
     pwd_email = dpg.get_value("pwd_email")
     items = dpg.get_item_configuration("lb_2")['items']
@@ -88,7 +63,7 @@ def send_email():
                              user[0], parent='text_parent', color=(255, 0, 0))
 
 
-def update_DB():
+def update_DB():  # функция обновления данных в БД
     if not (dpg.get_value("name") and dpg.get_value("login") and dpg.get_value("pwd")):
         dpg.add_text("Нету данных для соединения к Базе Данных",
                      parent='text_parent', color=(255, 0, 0))
@@ -99,12 +74,37 @@ def update_DB():
                      parent='text_parent', color=(0, 255, 0))
 
 
-def save_settings():
+def save_settings():  # функция сохранения настроек в файл
     settings.set_mssql(*dpg.get_values(['name', 'login', 'pwd']))
     settings.set_mail(dpg.get_value("login_email"), dpg.get_value("pwd_email"))
 
 
-dpg.create_viewport(title='Custom Title',
+settings = sett.Settings()  # создаём объект настроек
+
+dpg.create_context()  # создаём контекст
+
+with dpg.font_registry():  # загружаем шрифты
+    with dpg.font("NotoMono-Regular.ttf", 14, default_font=True) as font1:
+
+        # add the default font range
+        dpg.add_font_range_hint(dpg.mvFontRangeHint_Cyrillic)
+
+        # helper to add range of characters
+        #    Options:
+        #        mvFontRangeHint_Japanese
+        #        mvFontRangeHint_Korean
+        #        mvFontRangeHint_Chinese_Full
+        #        mvFontRangeHint_Chinese_Simplified_Common
+        #        mvFontRangeHint_Cyrillic
+        #        mvFontRangeHint_Thai
+        #        mvFontRangeHint_Vietnamese
+        dpg.add_font_range_hint(dpg.mvFontRangeHint_Cyrillic)
+
+width = GetSystemMetrics(0)  # получаем ширину системы
+height = GetSystemMetrics(1)  # получаем высоту системы
+
+
+dpg.create_viewport(title='Custom Title',  # заголовок окна
                     width=int(width/2),
                     height=int(height/2),
                     min_width=int(width/2.5),
@@ -115,28 +115,28 @@ dpg.create_viewport(title='Custom Title',
                     max_height=int(height/2)
                     )
 
-with dpg.viewport_menu_bar():
-    with dpg.menu(label="Settings"):
+with dpg.viewport_menu_bar():  # создаём меню
+    with dpg.menu(label="Settings"):  # создаём меню настроек
         with dpg.menu(label="Подключение к БД"):
             dpg.add_input_text(label="Имя Сервера", tag="name", default_value=settings.get_mssql()[
-                               0] if settings.is_exist else "")
+                               0] if settings.is_exist else "")  # поле ввода имени сервера
             dpg.add_input_text(label="Имя пользователя", tag="login", default_value=settings.get_mssql()[
-                               1] if settings.is_exist else "")
+                               1] if settings.is_exist else "")  # поле ввода имени пользователя
             dpg.add_input_text(label="Пароль", tag="pwd",
-                               password=True, hint="<password>", default_value=settings.get_mssql()[2] if settings.is_exist else "")
+                               password=True, hint="<password>", default_value=settings.get_mssql()[2] if settings.is_exist else "")  # поле ввода пароля
 
 
-with dpg.window(tag="start_window"):
+with dpg.window(tag="start_window"):  # создаём окно
 
-    with dpg.group(horizontal=True):
-        dpg.add_listbox(items=wwl.get_users(),
+    with dpg.group(horizontal=True):  # создаём группу
+        dpg.add_listbox(items=wwl.get_users(),  # создаём основной список абитуриентов
                         tag='lb_1',
                         callback=add_abit,
                         width=205,
                         pos=[535, 22],
                         num_items=18
                         )
-        dpg.add_listbox(items=[],
+        dpg.add_listbox(items=[],  # создаём второстепенный список абитуриентов
                         tag='lb_2',
                         callback=del_abit,
                         width=205,
@@ -144,21 +144,28 @@ with dpg.window(tag="start_window"):
                         num_items=18
                         )
 
+    # создаём окно для выбора файла
     with dpg.file_dialog(directory_selector=False, show=False, tag="file_dialog_id", width=700, height=400, callback=open_file):
         dpg.add_file_extension(".csv")
 
+    # создаём окно для вывода данных о абитуриентах
     with dpg.child_window(width=310, pos=[8, 22], height=334):
         pass
 
+    # создаём окно для логирования абитуриентов
     with dpg.child_window(width=310, pos=[8, 358], autosize_y=True, tag="text_parent"):
         pass
 
+    # создаём дочернее окно
     with dpg.child_window(width=420, pos=[320, 358], autosize_y=True):
-        with dpg.group(horizontal=True):
+        with dpg.group(horizontal=True):  # создаём группу
+            # создаём дочернее окно
             with dpg.child_window(width=198, pos=[8, 8], autosize_y=True):
+                # создаём группу с горизонтальной группировкой
                 with dpg.group(horizontal=True):
                     dpg.add_button(label='Открыть файл', width=94, height=50, tag='open', pos=[1, 4], callback=lambda: dpg.show_item(
-                        "file_dialog_id"), enabled=not os.path.isfile(os.getcwd()+'\\'+'my_db.db'))
+                        "file_dialog_id"), enabled=not os.path.isfile(os.getcwd()+'\\'+'my_db.db'))  # создаём кнопку для открытия файла
+                    # проверяем, есть ли база данных
                     if os.path.isfile(os.getcwd()+'\\'+'my_db.db'):
                         dpg.add_text('БД уже создана',
                                      parent='text_parent', color=(0, 255, 0))
@@ -166,23 +173,24 @@ with dpg.window(tag="start_window"):
                         dpg.add_text('БД не найдена, ... Выберите файл (.*csv)',
                                      parent='text_parent', color=(255, 0, 0))
                     dpg.add_button(label='Обновить', tag='update',
-                                   width=94, height=50, callback=lambda: threading.Thread(target=update_DB).start())
+                                   width=94, height=50, callback=lambda: threading.Thread(target=update_DB).start())  # создаём кнопку для обновления данных
                     dpg.add_button(label='Отправить',
-                                   tag="send_email", pos=[0, 61], width=95, height=50, callback=lambda: threading.Thread(target=send_email).start())
+                                   tag="send_email", pos=[0, 61], width=95, height=50, callback=lambda: threading.Thread(target=send_email).start())  # создаём кнопку для отправки писем
                     dpg.add_button(label='Сохранить \nнастройки',
-                                   tag="sett_save", width=95, height=50, callback=save_settings)
-            with dpg.child_window(width=198, autosize_y=True):
-                with dpg.group():
+                                   tag="sett_save", width=95, height=50, callback=save_settings)  # создаём кнопку для сохранения настройки
+            with dpg.child_window(width=198, autosize_y=True):  # создаём дочернее окно
+                with dpg.group():  # создаём группу
                     dpg.add_input_text(
-                        label="Логин", tag="login_email", width=128, default_value=settings.get_mail()[0] if settings.is_exist else "")
+                        label="Логин", tag="login_email", width=128, default_value=settings.get_mail()[0] if settings.is_exist else "")  # поле ввода логина пользователя
                     dpg.add_input_text(
-                        label="Пароль", tag="pwd_email", password=True, hint="<password>", default_value=settings.get_mail()[1] if settings.is_exist else "")
+                        label="Пароль", tag="pwd_email", password=True, hint="<password>", default_value=settings.get_mail()[1] if settings.is_exist else "")  # поле ввода пароля
 
-    dpg.bind_font(font1)
+    dpg.bind_font(font1)  # запускаем функцию для загрузки шрифта
 
-if __name__ == '__main__':
-    dpg.setup_dearpygui()
-    dpg.show_viewport()
+if __name__ == '__main__':  # точка входа в программу
+    dpg.setup_dearpygui()  # задаём настройки окна
+    dpg.show_viewport()  # отображаем окно
+    # делаем окно в основном окне главным
     dpg.set_primary_window("start_window", True)
-    dpg.start_dearpygui()
-    dpg.destroy_context()
+    dpg.start_dearpygui()  # запускаем программу
+    dpg.destroy_context()  # останавливаем контекст
