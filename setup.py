120deb7eca998fefc9ac64a10b0dc0ddf33c26aa
Дмитрий <dolgov.mitia2016@yandex.ru>
2024-07-02 00:47:54 +0700

    изменён setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from cx_Freeze import setup, Executable
 
 build_exe_options = {
+    "excludes": ['unicodedata', 'logging', 'unittest', 'http', 'xml', 'bz2','xmlrpc','tkinter','tk8.6'],
     "include_files": ["NotoMono-Regular.ttf", "message.html"],
+    "zip_include_packages": ['dearpygui','csv','sqlite3','win32api','os','Thread','configparser','smtplib','email','pyodbc','urllib']
 }
 
 setup(
@@ -9,5 +11,5 @@ setup(
     version="1.0",
     description="Отправка уведомлений об экзаменах",
     options={"build_exe": build_exe_options},
-    executables=[Executable("main.py", base="Win32GUI", target_name="SoE.exe")]
+    executables=[Executable("main.py", base="Win32GUI", target_name="SoE.exe",icon='exam.ico')]
 )
